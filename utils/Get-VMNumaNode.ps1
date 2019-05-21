2715a1c23396ad7321f39a445098564a7e1de02c
Simon Buckner <simon@onebyte.net>
2019-05-21 12:25:54 +0100

    Tidy u the sript and apply the script template

--- utils/Get-VMNumaNode.ps1
+++ utils/Get-VMNumaNode.ps1
@@ -1,20 +1,72 @@
+<#
+.SYNOPSIS
+	List VMs by NUMA node
+	
+.DESCRIPTION
+	Examine the runnig Hyper-V guest machines and return the NUMA node
+	each one is running on
 
-Class VM {
-	[string]$Hostname
-	[int]$NumaNode
+.EXAMPLE
+  .\Get-VMNumaNode.ps1
+ 
+  Lists VMs by NUMA node
+
+.NOTES
+  Authored By: Simon Buckner
+  Email: simon@onebyte.net
+  Date: 21st May 2019
+
+  .LINK
+  https://onebyte.eu.itglue.com/269174305390819/docs/1121244393242834#documentMode=edit&version=published
+
+#>
+
+[CmdletBinding(SupportsShouldProcess = $True)]
+Param(
+)
+
+BEGIN {
+
+    # Run one-time set-up tasks here, like defining variables, etc.
+    Set-StrictMode -Version Latest
+    Write-Verbose -Message "$($MyInvocation.MyCommand.Name): Started."
+
+    # Declare any classes used later in the sript
+    # #########################################################################
+    Class VM {
+        [int]$NumaNode
+        [string]$Hostname
+
+        VM($NumaNode, $Hostname) {
+            $this.NumaNode = $NumaNode
+            $this.Hostname = $Hostname
+        }
+    }
 }
 
-$samples = Get-Counter -Counter "\Hyper-V VM Vid Partition(*)\Preferred NUMA Node Index"
+Process {
+    # Place all script elements within the process block to allow processing of
+    # pipeline correctly.
+    
+    $counters = Get-Counter -Counter "\Hyper-V VM Vid Partition(*)\Preferred NUMA Node Index"
 
-$vms = @()
+    $vms = @()
 
-foreach ($sample in $samples.CounterSamples) {
-	if ($sample.InstanceName -ne "_total") {
-		$newVm = New-Object VM
-		$newVm.Hostname = $sample.InstanceName
-		$newVm.NumaNode = $sample.CookedValue
-		$vms += $newVm
-	}
+    foreach ($sample in $counters.CounterSamples) {
+        if ($sample.InstanceName -ne "_total") {
+            $newVm = [VM]::new($sample.CookedValue, $sample.InstanceName)
+            $vms += $newVm
+        }
+    }
+
+    $vms | Sort-Object -Property NumaNode, Hostname
 }
 
-$vms | Sort-Object -Property Hostname
+END {       
+    # Finally, run one-time tear-down tasks here.
+    Write-Verbose -Message "$($MyInvocation.MyCommand.Name): Complete."
+}
+
+
+
+
