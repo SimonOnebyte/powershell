304b931a737fd90251410bb30e249699e9151ff0
Simon Buckner <simon@onebyte.net>
2019-07-18 15:02:24 +0100

    Make Check-MAC accept any format mac.txt file
    TODO:
      Wrap in standard skeleton
      Add proper help text

--- utils/Check-ARP.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-IF NOT EXIST "C:\Program Files\PowerShell\6\pwsh.exe" (
-    echo Please install PowerSehll 6 from https://github.com/PowerShell/PowerShell
-    set /p Cont="Press enter to close window"
-    exit 1
-)
-"C:\Program Files\PowerShell\6\pwsh.exe" -file .\Check-MACs.ps1 -ARP
--- utils/Check-MACs.ps1
+++ utils/Check-MACs.ps1
@@ -5,54 +5,29 @@
 #
 # Paste the result into a file called macs.txt in this directory
 # Then run this script. It will output a list of MACs & IP's along with the manufacturer
-param (
-    [switch]$ARP=$false
-)
-
-class Host {
-    [string]$MAC
-    [string]$IP
-}
-
-function NewHost($mac, $ip) {
-    $h = [Host]::new()
-    $h.MAC = $mac
-    $h.IP = $ip
-    $h
-}
+# param (
+#     [switch]$ARP=$false
+# )
 
 $apiKey = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJtYWN2ZW5kb3JzIiwiZXhwIjoxODM4MTAyMzgzLCJpYXQiOjE1MjM2MDYzODMsImlzcyI6Im1hY3ZlbmRvcnMiLCJqdGkiOiI3NDhmMWQxNi04NmQyLTRkNjctODFkMS1hMDdmOWFiZjdjMjgiLCJuYmYiOjE1MjM2MDYzODIsInN1YiI6IjIzMCIsInR5cCI6ImFjY2VzcyJ9.6NhgKTluSu8T2Hy-fsLofnG-O6XagGksBSg-fr-cTW9RYgQfQzWeqLqdsnssBSNUZJEJe-fPhzz3Kj57cEaDVQ"
 $token = ConvertTo-SecureString -String $apiKey -AsPlainText -Force
 
-$hosts = @()
+$MACreg = "([0-9A-F]{2}[:-]){5}([0-9A-F]{2})"
 
-if ($ARP) {
-    $arpCols = 25, 34, 54, 76
-    $delim = ","
-    $content = Get-Content ".\arp.txt"
-    foreach ($line in $content) {
-        foreach ($c in $arpCols) {
-            $line = $line.Insert($c, $delim)
-        }
-        $parts = $line.Replace("  ", "").Split(",")
-        $hosts += (NewHost $parts[2] $parts[0])
-    }
-} else {
-    $macs = Import-Csv -Path ".\macs.txt" -Header "IP","HWtype","MAC","Flag","Iface" -Delimiter "`t" | Sort-Object Mac
-    foreach ($mac in $macs) {
-        $hosts += (NewHost $mac.MAC $mac.IP)
-    }
+$matches = Select-String -Path ".\macs.txt" -Pattern $MACreg
+
+$macs = @()
+foreach ($match in $matches) {
+    if ($match.Matches.Value -NotMatch "00:00:00:00:00:00") {
+        $macs += $match.Matches.Value
+    } 
 }
-
-
 
 $url = "https://api.macvendors.com/v1/lookup"
 
-foreach ($h in $hosts) {
-    $mac = $($h.MAC)
-    $ip = $($h.IP)
+foreach ($mac in $macs) {
 
-    Write-Host "Checking $mac - $ip `t: " -NoNewline
+    Write-Host "Checking  $mac  : " -NoNewline
     try {
         $res = Invoke-RestMethod -Uri "$url/$mac)" -Authentication Bearer -Token $token -Headers @{"Accept"="text/plain"}
         Write-Host $res
