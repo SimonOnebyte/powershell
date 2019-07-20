232bb3e67309fe7d60602d9f151b16687a05ddba
SimonBuckner <simonbuckner@hotmail.com>
2019-07-20 09:06:31 +0100

    Create Deploy-Script and update default profile

---
+++ profile/Deploy-Script.ps1
@@ -0,0 +1,101 @@
+<#
+.SYNOPSIS
+  Deploys a script local profile directory for dot sourcing into profile
+
+.DESCRIPTION
+  First checks that there is a profile setup and creates if it isn't. Then 
+  copies the specified script(s) into the 'myScripts' directory in the same
+  directory as the profile directory.
+
+  Each script added is checked to ensure the first line is a 'function' 
+  statement and if it is not, the entire script is wrapped ina function 
+  whose name is the same as the filename. 
+
+.PARAMETER Path
+  The script or scripts that you want dot sources by the local profile
+
+.EXAMPLE
+  DeployScript -Path .\My-Script.ps1
+ 
+  Will copy the script 'My-Script' into the profile so it is always available
+
+.NOTES
+  Authored By: Simon Buckner
+  Email: simon@onebyte.net
+  Date: 20th July 2019
+
+#>
+
+# [CmdletBinding(SupportsShouldProcess = $True)]
+Param(
+  [Parameter(
+    Mandatory = $True,
+    Position = 0,
+    ValueFromPipeline = $True,
+    ValueFromPipelineByPropertyName = $True
+  )]
+  [ValidateLength(1, 256)]
+  [String[]]$Path
+)
+
+BEGIN {
+
+  # Run one-time set-up tasks here, like defining variables, etc.
+  Set-StrictMode -Version Latest
+  Write-Verbose -Message "$($MyInvocation.MyCommand.Name): Started."
+  $myScripts = "$(Split-Path -Path $profile -Parent)\myScripts"
+
+  $prof = @"
+function Global:prompt { "PS [`$Env:username]`$PWD`n>" } 
+
+`$path = Split-Path -Path `$PROFILE
+
+if (Test-Path -Path "`$path\myScripts") {
+  Get-ChildItem -Path "`$path\myscripts" -Filter *.ps1 | ForEach-Object {
+    . `$_.FullName
+  }
+}
+"@
+
+  Write-Verbose "Checking to see if a profile exists"
+  if (-not (Test-Path $profile)) {
+    Write-Verbose "Creating profile"
+    New-Item -Path $profile -Force | Out-Null
+    $prof | Out-File -FilePath $profile
+  }
+
+  Write-Verbose "Checking to see if myScripts folder exists"
+  if (-not (Test-Path $myScripts)) {
+    Write-Verbose "Creating myScripts folder"
+    New-Item -Path $myScripts -ItemType Directory | Out-Null
+  }
+}
+
+
+Process {
+  # Place all script elements within the process block to allow processing of
+  # pipeline correctly.
+    
+  # The process block can be executed multiple times as objects are passed through the pipeline into it.
+  ForEach ($item In $Path) {
+    Write-Verbose "Deploying -Param1:$item"
+
+    $script = Get-Content -Path $item
+    if ($script[0] -notmatch "^[fF]unction") {
+      $name = Split-Path -Path $item -LeafBase
+      Write-Verbose "Wrapping $name in a function"
+      $scriptOut = "function $name {`n"
+      foreach ($l in $script) {
+        $scriptOut += "$l`n"
+      }
+      $scriptOut += "`n}"
+      $script = $scriptOut
+    }
+    $script | Out-File -FilePath "$myScripts\$name.ps1" -Force
+  }
+}
+
+END {       
+  # Finally, run one-time tear-down tasks here.
+  Write-Verbose -Message "$($MyInvocation.MyCommand.Name): Complete."
+}
--- profile/Microsoft.PowerShell_profile.ps1.ps1
+++ profile/Microsoft.PowerShell_profile.ps1.ps1
@@ -1,56 +1,9 @@
-function Connect-Office365 {
-    param (
-        # Key for the client portal to connect to
-        [Parameter(Mandatory = $TRUE, Position = 0, ValueFromPipeline = $TRUE, ParameterSetName = "connect")]
-        [string]$Key,
+function Global:prompt { "PS [$Env:username]$PWD`n>" } 
 
-        [Parameter(Mandatory = $FALSE, Position = 1, ValueFromPipeline = $FALSE, ParameterSetName = "list")]
-        [switch]$List
-    
-    )
+$path = Split-Path -Path $PROFILE
 
-    # Load list of client portals
-    $cfgFile = "$($ENV:ProgramData)\Onebyte\office365.json"
-
-    if (-not (Test-Path -Path $cfgFile)) {
-        Write-Error "Uanble to find $cfgFile"
-        return 1
-    }
-
-    $o365 = (Get-Content $cfgFile) -join "`n" | ConvertFrom-Json
-
-    if ($List) {
-        $o365.portals | Format-Table
-        return 
-    }
-    $found = $FALSE
-
-    foreach ( $portal in $o365.portals ) {
-        if ( $portal.key -like $Key ) {
-            $found = $TRUE
-            Write-Host "Connecting to $($portal.client) Office 365 portal"
-
-            $cred = Get-Credential -UserName $portal.account -Message "Enter password for $client"
-
-            Write-Verbose "Connecting to MS Online Service"
-            Connect-MsolService 
-            # -Credential $cred
-        
-            Write-Verbose "Connecting to Azure AD"
-            Connect-AzureAD 
-            # -Credential $cred
-
-            Write-Verbose "Connecting to MS Exchange Online"
-            $exchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $cred -Authentication "Basic" -AllowRedirection
-            # -Credential $cred -Authentication "Basic" -AllowRedirection
-            Import-PSSession $exchangeSession
-
-            break
-        }
-    }
-
-    if ( $found -ne $TRUE ) {
-        Write-Error "Unable to find details for $key"
-        return 1
-    }
+if (Test-Path -Path "$path\myScripts") {
+  Get-ChildItem -Path "$path\myscripts" -Filter *.ps1 | ForEach-Object {
+    . $_.FullName
+  }
 }
