79c1a44a15923bccafa3df0e0b80298c521ecbaf
Simon Buckner <simon@onebyte.net>
2019-07-24 11:21:30 +0100

    Bug: Fix problems from first real world test
    Now checks for the correct execution policy
    Unblocks the scripts before copying them

--- 0. published/Deploy-All.cmd
+++ 0. published/Deploy-All.cmd
@@ -17,6 +17,22 @@ Write-Host "overwritten and any customisations will be lost"
 Write-Host ""
 Write-Host ""
 
+$ep = Get-ExecutionPolicy
+if ($ep -ne """Unrestricted""") {
+    Write-Host ""
+    Write-Host "Before you can run this script, please do the followning."
+    Write-Host ""
+    Write-Host "1.  Launch PowerShell using 'Run as Administrator'"
+    Write-Host "2.  Paste the following commmand into the window"
+    Write-Host ""
+    Write-Host "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force"
+    Write-Host ""
+    Write-Host "3.  Run this script again"
+
+    Read-Host "Press enter to close this window"
+    Exit
+}
+
 $scripts = Get-ChildItem -Path "." -Filter "*.ps1" | ForEach-Object { 
     if ($_.Name -ne """Microsoft.PowerShell_profile.ps1""" -and $_.Name -ne """Deploy-AllPowerShell6.ps1""") {
         $_
@@ -32,11 +48,14 @@ Write-Host ""
 $r = Read-Host "Type 'yes' and press enter to continue."
 
 if ($r -match 'yes') {
+    Unblock-File -Path ".\Deploy-Script.ps1"
+
     $myScripts = (Split-Path -Path $profile -Parent)
     Remove-Item -Path """$myScripts\myScripts\*.ps1"""
     $force = $true
     foreach ($script in $scripts) {
         Write-Host "- -->Deploying $($script.Name)"
+        Unblock-File -Path $script
         if (-not $force) {
             .\Deploy-Script.ps1 -Path """.\$($script.File)"""
         }
