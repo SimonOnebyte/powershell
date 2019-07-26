5e6d5fa9272e196360f4f17091d516f4d05117f6
Simon Buckner <simon@onebyte.net>
2019-07-26 09:48:10 +0100

    No longer require the CMD file

--- workstation/Get-MACVendors.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-IF NOT EXIST "C:\Program Files\PowerShell\6\pwsh.exe" (
-    echo Please install PowerSehll 6 from https://github.com/PowerShell/PowerShell
-    set /p Cont="Press enter to close window"
-    exit 1
-)
-
-"C:\Program Files\PowerShell\6\pwsh" -File .\Get-MACVendors.ps1 -InFile .\macs.txt
